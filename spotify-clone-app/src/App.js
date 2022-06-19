ae3b9b310e870068c55ebbcbf79a8a514a3b225e
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 02:27:50 +0530

    Update App.js

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+  
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log("🧔🏻", user);
+      })
+    }
+    console.log("Got a token 👉🏻:", token);
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
