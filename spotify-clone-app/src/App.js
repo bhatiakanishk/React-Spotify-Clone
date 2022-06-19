34e0fc2d25b80a79d8add2e74042647c7d33e319
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 02:56:24 +0530

    Update App.js

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -4,11 +4,13 @@ import Login from './Login';
 import Player from './Player';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
   
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -17,11 +19,15 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      
 
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log("🧔🏻", user);
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
       })
     }
     console.log("Got a token 👉🏻:", token);
