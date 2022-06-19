87940e8e16b88a1d6c76f14d9c37adb342022e4c
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-19 21:41:06 +0530

    Update App.js

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Login />
     </div>
   );
 }
