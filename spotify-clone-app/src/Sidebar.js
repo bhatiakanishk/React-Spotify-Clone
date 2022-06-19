fa8903f7e55a24271fddff541ee99bb6e8a42d0d
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 04:04:09 +0530

    Update Sidebar.js

--- spotify-clone-app/src/Sidebar.js
+++ spotify-clone-app/src/Sidebar.js
@@ -11,11 +11,12 @@ function Sidebar() {
       <img
         className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
-      <SidebarOption Icon={HomeIcon} option="Home" />
-      <SidebarOption Icon={SearchIcon} option="Search" />
-      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+      <SidebarOption Icon={HomeIcon} option="Home" title="Home"/>
+      <SidebarOption Icon={SearchIcon} option="Search" title="Search"/>
+      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" title="Your Library"/>
       <br />
       <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
     </div>
   )
 }
