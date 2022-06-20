f7296423de3e6c1add3d4476eaf21db1fa558a50
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 15:25:58 +0530

    Update Player.js

--- spotify-clone-app/src/Player.js
+++ spotify-clone-app/src/Player.js
@@ -1,34 +1,19 @@
-import React from 'react';
-import './Player.css';
-import SidebarOption from "./SidebarOption";
-import { getTokenFromResponse } from "./spotify";
-import { useStateValue } from "./StateProvider";
-import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import React from "react";
+import Footer from "./Footer";
+import "./Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
 
-function Sidebar() {
-  const [{ playlists }, dispatch] = useStateValue();
-  console.log(playlists);
-
+function Player({ spotify }) {
   return (
-    <div className="sidebar">
-      <img
-        className="sidebar__logo"
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt=""
-      />
-      <SidebarOption Icon={HomeIcon} option="Home" />
-      <SidebarOption Icon={SearchIcon} option="Search" />
-      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
-      <br />
-      <strong className="sidebar__title">PLAYLISTS</strong>
-      <hr />
-      {playlists?.items?.map((playlist) => (
-        <SidebarOption option={playlist.name} />
-      ))}
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer spotify={spotify} />
     </div>
   );
 }
 
-export default Sidebar;
+export default Player;
