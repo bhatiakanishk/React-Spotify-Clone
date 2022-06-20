1fbcf7f2351e6f8642f4045f7f68a97072f7f8e0
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 14:35:14 +0530

    Update reducer.js

--- spotify-clone-app/src/reducer.js
+++ spotify-clone-app/src/reducer.js
@@ -14,12 +14,17 @@ const reducer = (state, action) => {
                 return {
                     ...state,
                     user: action.user
-                }
+                };
             case 'SET_TOKEN':
                 return {
                     ...state,
                     token: action.token
-                }
+                };
+            case 'SET_PLAYLISTS':
+                return {
+                    ...state,
+                    playlists: action.playlists,
+                };
         default:
             return state;
     }
