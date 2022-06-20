867de32e2a9bf8701365fdc0d43086d01d90e065
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 15:25:52 +0530

    Update Login.js

--- spotify-clone-app/src/Login.js
+++ spotify-clone-app/src/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Login.css';
-import { loginUrl } from './spotify';
+import { accessUrl } from './spotify';
 
 function Login() {
   return (
     <div className="login">
         <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="logo"/>
-        <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
+        <a href={accessUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   )
 }
