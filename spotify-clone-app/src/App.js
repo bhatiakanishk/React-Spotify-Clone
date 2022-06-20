6e72a3d67ed8ebc53324bcd4ac6f80ae1c41e6e1
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 15:37:16 +0530

    Update App.js

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import { useStateValue } from "./StateProvider";
-import Player from './Player';
+import Player from "./Player";
 import { getTokenFromResponse } from "./spotify";
-import './App.css';
-import Login from './Login';
+import "./App.css";
+import Login from "./Login";
 
 const s = new SpotifyWebApi();
 
