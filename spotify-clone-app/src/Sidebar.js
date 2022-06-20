3f1ba0d6fa7aa5cba3c98cb1c7230606dc34d9fe
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 14:52:29 +0530

    Update Sidebar.js

--- spotify-clone-app/src/Sidebar.js
+++ spotify-clone-app/src/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Sidebar.css';
 import SidebarOption from './SidebarOption';
-import { useStateValue } from "./StateProvider";
+//import { useStateValue } from "./StateProvider";
 import { useDataLayerValue } from './DataLayer';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
