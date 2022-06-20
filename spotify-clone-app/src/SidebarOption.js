c2fa5c4441d950a67044d5f874be6878d1270646
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 15:25:54 +0530

    Update SidebarOption.js

--- spotify-clone-app/src/SidebarOption.js
+++ spotify-clone-app/src/SidebarOption.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SidebarOption.css';
 
-function SidebarOption({ title, Icon}) {
+function SidebarOption({ option = "test", Icon }) {
   return (
     <div className="sidebarOption">
       {Icon && <Icon className="sidebarOption__icon" />}
