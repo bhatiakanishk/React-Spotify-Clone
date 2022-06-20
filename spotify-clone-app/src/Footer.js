c5cef7b71089741234bb59b13d010100df497e79
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 15:03:24 +0530

    Update Footer.js

--- spotify-clone-app/src/Footer.js
+++ spotify-clone-app/src/Footer.js
@@ -1,26 +1,118 @@
-import React from 'react'
+
+import React, { useEffect, useState } from "react";
 import './Footer.css';
+import { useStateValue } from "./StateProvider";
 import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
+import PauseCircleOutlineOutlinedIcon from '@mui/icons-material/PauseCircleOutlineOutlined';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
 import RepeatIcon from '@mui/icons-material/Repeat';
-import PlayCircleOutlined from '@mui/icons-material/PlayCircleOutlined';
 import { Grid, Slider } from '@mui/material';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
 import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 
-function Footer() {
-  return (
-    <div className="Footer">
-      <div className="footer__left">
+function Footer({ spotify }) {
+  const [{ token, item, playing }, dispatch] = useStateValue();
 
+  useEffect(() => {
+    spotify.getMyCurrentPlaybackState().then((r) => {
+      console.log(r);
+
+      dispatch({
+        type: "SET_PLAYING",
+        playing: r.is_playing,
+      });
+
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+    });
+  }, [spotify]);
+
+  const handlePlayPause = () => {
+    if (playing) {
+      spotify.pause();
+      dispatch({
+        type: "SET_PLAYING",
+        playing: false,
+      });
+    } else {
+      spotify.play();
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    }
+  };
+
+  const skipNext = () => {
+    spotify.skipToNext();
+    spotify.getMyCurrentPlayingTrack().then((r) => {
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    });
+  };
+
+  const skipPrevious = () => {
+    spotify.skipToPrevious();
+    spotify.getMyCurrentPlayingTrack().then((r) => {
+      dispatch({
+        type: "SET_ITEM",
+        item: r.item,
+      });
+      dispatch({
+        type: "SET_PLAYING",
+        playing: true,
+      });
+    });
+  };
+
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src={item?.album.images[0].url}
+          alt={item?.name}
+        />
+        {item ? (
+          <div className="footer__songInfo">
+            <h4>{item.name}</h4>
+            <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
+          </div>
+        ) : (
+          <div className="footer__songInfo">
+            <h4>No song is playing</h4>
+            <p>...</p>
+          </div>
+        )}
       </div>
+
       <div className="footer__center">
-        <ShuffleIcon className="footer__green"/>
-        <SkipPreviousIcon className="footer__icon" />
-        <PlayCircleOutlined fontSize="large" className="footer__icon"/>
-        <SkipNextIcon className="footer__icon" />
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon onClick={skipNext} className="footer__icon" />
+        {playing ? (
+          <PauseCircleOutlineOutlinedIcon
+            onClick={handlePlayPause}
+            fontSize="large"
+            className="footer__icon"
+          />
+        ) : (
+          <PlayCircleOutlinedIcon
+            onClick={handlePlayPause}
+            fontSize="large"
+            className="footer__icon"
+          />
+        )}
+        <SkipNextIcon onClick={skipPrevious} className="footer__icon" />
         <RepeatIcon className="footer__green" />
       </div>
       <div className="footer__right">
@@ -32,12 +124,12 @@ function Footer() {
             <VolumeDownIcon />
           </Grid>
           <Grid item xs>
-            <Slider aria-labelledby="continuous-slider"/>
+            <Slider aria-labelledby="continuous-slider" />
           </Grid>
         </Grid>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
