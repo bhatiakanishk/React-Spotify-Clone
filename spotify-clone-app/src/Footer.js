99766c11290a322cba0a026d1968a3b7ccdb9d4e
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2022-06-20 14:52:27 +0530

    Update Footer.js

--- spotify-clone-app/src/Footer.js
+++ spotify-clone-app/src/Footer.js
@@ -1,10 +1,41 @@
 import React from 'react'
 import './Footer.css';
+import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import PlayCircleOutlined from '@mui/icons-material/PlayCircleOutlined';
+import { Grid, Slider } from '@mui/material';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
 
 function Footer() {
   return (
     <div className="Footer">
-        <h1>Footer</h1>
+      <div className="footer__left">
+
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green"/>
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlined fontSize="large" className="footer__icon"/>
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider"/>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
